c198b1159f3d4db36f96babb8e5d3b1a77b599fd
Ellen Liao <ellen.liao.usa@gmail.com>
2024-11-12 23:11:53 -0600

    Revert enum methods
    Reverts enum methods to calling `EnumName.VALUE` instead of `cls.VALUE`.

--- enums.py
+++ enums.py
@@ -9,8 +9,8 @@ class RecipeSource(Enum):
     @classmethod
     def from_url(cls, url: str):
         if re.findall(r'allrecipes\.com/recipe/.*', url):
-            return cls.ALLRECIPES
-        return cls.UNKNOWN
+            return RecipeSource.ALLRECIPES
+        return RecipeSource.UNKNOWN
 
 class HTMLTag(Enum):
     '''Enum of relevant HTML tag types'''
@@ -27,27 +27,27 @@ class HTMLTag(Enum):
     def __from_allrecipes_tag(cls, tag: str, attrs: list[tuple[str, str | None]]):
         if tag == 'div':
             if attrs == [('class', 'mm-recipes-details__label')]:
-                return cls.OVERVIEW_LABEL
+                return HTMLTag.OVERVIEW_LABEL
             elif attrs == [('class', 'mm-recipes-details__value')]:
-                return cls.OVERVIEW_TEXT
+                return HTMLTag.OVERVIEW_TEXT
         elif tag == 'li':
             if ('class', 'mm-recipes-structured-ingredients__list-item ') in attrs:
-                return cls.INGREDIENT
+                return HTMLTag.INGREDIENT
         elif tag == 'span':
             if attrs == [('data-ingredient-quantity', 'true')]:
-                return cls.INGREDIENT_QUANTITY
+                return HTMLTag.INGREDIENT_QUANTITY
             elif attrs == [('data-ingredient-unit', 'true')]:
-                return cls.INGREDIENT_UNIT
+                return HTMLTag.INGREDIENT_UNIT
             elif attrs == [('data-ingredient-name', 'true')]:
-                return cls.INGREDIENT_NAME
+                return HTMLTag.INGREDIENT_NAME
         elif tag == 'p':
             if ('class', 'comp mntl-sc-block mntl-sc-block-html') in attrs:
-                return cls.STEP
-        return cls.UNKNOWN
+                return HTMLTag.STEP
+        return HTMLTag.UNKNOWN
 
     @classmethod
     def from_tag(cls, source: RecipeSource, tag: str, attrs: list[tuple[str, str | None]]):
         match source:
             case RecipeSource.ALLRECIPES:
-                return cls.__from_allrecipes_tag(tag, attrs)
-        return cls.UNKNOWN
+                return HTMLTag.__from_allrecipes_tag(tag, attrs)
+        return HTMLTag.UNKNOWN
